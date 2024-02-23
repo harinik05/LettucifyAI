b4bde3b6d392c9585103a0bd74610043e7f3f2f0
harinik05 <hka747910@gmail.com>
2024-02-23 17:40:16 -0500

    updated gpu training task

---
+++ SRC/Model_Training_Sequence.py
@@ -0,0 +1,8 @@
+###################################
+# Name: Harini Karthik
+# File_Name: Model_Training_Sequence
+# Parent_File: Training.py
+# Date: Feb. 23, 2024
+# Reference: AzureML Sample Directory
+###################################
+
