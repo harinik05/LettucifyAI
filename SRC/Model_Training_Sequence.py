343fa3cb80d019a895f99f45f5b89a524e04d3be
harinik05 <hka747910@gmail.com>
2024-02-24 21:56:58 -0500

    epoch evaluation

--- SRC/Model_Training_Sequence.py
+++ SRC/Model_Training_Sequence.py
@@ -245,15 +245,20 @@ class Model_Training_Sequence:
                    images = images.to(self.device, non_blocking = self.dataloading_config.non_blocking)
                    one_hot_targets = targets.to(self.device, non_blocking = self.dataloading_config.non_blocking)
             
-                # 
+                # forward pass - Pass the image thruogh the model to optain outputs
                 with record_function("eval.forward"):
                    outputs = self.model(images)
+
+                   # calculate the loss and multiply by batch size
                    loss = criterion(outputs, one_hot_targets)
                    running_loss += loss.item()*images.size(0)
 
+                   # Compares the index of the max value in predicted output with target label 
                    correct = torch.argmax(outputs,dim=-1) == (targets.to(self.device))
                    num_correct += torch.sum(correct).item()
                    num_total_images += len(images)
 
                 return running_loss, num_correct, num_total_images
+    
+    
     
